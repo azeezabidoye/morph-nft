3cd4b84627468745a8b2c37db13da84cf857a6df
azeezabidoye <azeezabidoye@yahoo.com>
2024-10-08 11:50:37 +0100

    Config updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    morphL2: {
+      url: "https://rpc-quicknode-holesky.morphl2.io",
+      accounts: [process.env.PRIVATE_KEY], // Store your private key in .env file for security
+    },
+  },
 };
