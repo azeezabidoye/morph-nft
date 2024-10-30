5108a65796eee426ca20f31ce163215431861e85
azeezabidoye <azeezabidoye@yahoo.com>
2024-10-30 15:07:19 +0100

    Hardhat Config updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,9 @@ module.exports = {
   networks: {
     morphL2: {
       url: "https://rpc-quicknode-holesky.morphl2.io",
-      accounts: [process.env.PRIVATE_KEY], // Store your private key in .env file for security
+      accounts: [
+        `0xa77868cba9d67ed2854547cdebb3e30c52cabaa1c4646beef008e786c811c5fc`,
+      ], // Store your private key in .env file for security
     },
   },
 };
